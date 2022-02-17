a5727dac6ec5d1d281c050d2f4c4754b250d8c4b
vasvap <vasvap@gmail.com>
2022-02-17 09:05:32 +0300

    change org.springframework.boot to 2.5.3

--- src/main/java/guru/qa/restbackend/RestBackendApplication.java
+++ src/main/java/guru/qa/restbackend/RestBackendApplication.java
@@ -5,7 +5,10 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 
 @SpringBootApplication
@@ -19,7 +22,7 @@ public class RestBackendApplication {
 	public ObjectMapper jacksonMapper() {
 		return new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
 	}
- /*
+
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
@@ -28,6 +31,4 @@ public class RestBackendApplication {
 				.paths(PathSelectors.any())
 				.build();
 	}
-
- */
 }
